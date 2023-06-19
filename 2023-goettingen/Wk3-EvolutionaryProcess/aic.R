b0d6e8e26579efa822c03fbf06dc63988126e551
andrew-hipp <andrew.hipp@gmail.com>
2023-06-19 09:50:28 +0200

    finalized tutorials, week 3

--- 2023-goettingen/Wk3-EvolutionaryProcess/aic.R
+++ 2023-goettingen/Wk3-EvolutionaryProcess/aic.R
@@ -1,10 +1,25 @@
-function (x, ...) 
+function (x = NA, ...) 
 {
-    L <- logLik(x)
+    if (!hasArg(L)) {
+        L <- logLik(x)
+    }
+    else {
+        L = list(...)$L
+    }
     dev <- -2 * L
-    K = attr(L, "df")
-    n = attr(L, "nall")
+    if (!hasArg(K)) {
+        K = attr(L, "df")
+    }
+    else {
+        K = list(...)$K
+    }
     aic <- dev + 2 * K
+    if (!hasArg(n)) {
+        n = attr(L, "nall")
+    }
+    else {
+        n = list(...)$n
+    }
     aic.c <- aic + (2 * K * (K + 1))/(n - K - 1)
     out <- list(aic = aic, aic.c = aic.c, n = n, K = K, lnL = L)
     if ("modelStruct" %in% names(x)) 
